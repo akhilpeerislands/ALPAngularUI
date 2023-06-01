c6da28ce839f08d0e9dd2471a3ee5f6eb3828694
Akhil Kumar PeerIslands <akhilkumar.penugonda@peerislands.io>
2023-06-01 14:53:03 -0500

    Added Local development Changes

---
+++ sakai-ng-LocalDevelopment/src/app/demo/service/country.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CountryService {
+
+    constructor(private http: HttpClient) { }
+
+    getCountries() {
+        return this.http.get<any>('assets/demo/data/countries.json')
+            .toPromise()
+            .then(res => res.data as any[])
+            .then(data => data);
+    }
+
+    getBTs() {
+        return this.http.get<any>('assets/demo/data/Bts.json')
+            .toPromise()
+            .then(res => res.data as any[])
+            .then(data => data);
+    }
+}
